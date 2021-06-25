e602f32f8636d0fd2e70a4a29ed7d68a15fa86a2
yuen26 <nguyentuananh11b6@gmail.com>
2021-06-25 20:03:54 +0900

    Update faker

--- backend/faker/src/main/java/org/ashina/ecommerce/faker/FakerApplication.java
+++ backend/faker/src/main/java/org/ashina/ecommerce/faker/FakerApplication.java
@@ -1,6 +1,5 @@
 package org.ashina.ecommerce.faker;
 
-import org.ashina.ecommerce.faker.model.product.Product;
 import org.ashina.ecommerce.faker.repository.product.ProductRepository;
 import org.ashina.ecommerce.faker.service.ProductFakerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class FakerApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProductFakerService productFakerService;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	private static final Random random = new Random();
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakerApplication.class, args);
@@ -20,6 +26,10 @@ public class FakerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		productFakerService.fake(10);
+//		productFakerService.fake(1000);
+		while (true) {
+			int randomNumber = random.nextInt(100 - 1 + 1) + 1;
+			productRepository.findByPriceBetween(randomNumber, randomNumber + 10);
+		}
 	}
 }
